045305c8e5547fc2f54a2ed7aba48a089b5ddcfe
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-08 17:13:56 -0400

    autosave: 2026-09-08 17:13:55

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ export { envoyerInfolettre } from './infolettre/send';
 export { desabonner } from './infolettre/unsubscribe';
 export { resendWebhook } from './infolettre/webhook';
 export { creerPaiementFacture, webhookStripeXena } from './factures/paiement';
+export { synchroniserProduitStripe, creerPaiementProduit, webhookStripeProduits } from './produits/stripe';
 export {
   agendaGoogleConnecter,
   agendaGoogleRetour,
