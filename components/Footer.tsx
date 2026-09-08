4c422377dd524116a5f91c283fb8d69707ba843c
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-08 00:48:24 -0400

    autosave: 2026-09-08 00:48:23

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,6 +13,7 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({ onAdminLogin, lang, onChangeView }) => {
   const lenis = useLenis();
+  const bandeauVisible = useConsentementVisible();
   const t = {
     FR: {
       tagline: 'Consultante en carrière artistique et en communication.',
