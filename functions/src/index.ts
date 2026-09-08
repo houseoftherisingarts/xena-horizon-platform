08752499a9655c5951baa4a959ece90af9f48fba
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-08 17:03:53 -0400

    autosave: 2026-09-08 17:03:53

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,11 @@
 export { envoyerInfolettre } from './infolettre/send';
 export { desabonner } from './infolettre/unsubscribe';
 export { resendWebhook } from './infolettre/webhook';
+export {
+  agendaGoogleConnecter,
+  agendaGoogleRetour,
+  agendaGoogleEtat,
+  agendaGoogleDeconnecter,
+  agendaGoogleSync,
+  agendaGoogleSyncSurEcriture,
+} from './agenda/google';
